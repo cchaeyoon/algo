ce0caaf777427088d503905bb6a37120647399c7
cchaeyoon <161849407+cchaeyoon@users.noreply.github.com>
2025-01-16 23:15:38 +0900

    [Bronze III] Title: 공 넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int n, m;
+	cin >> n >> m;
+	vector<int> arr(n);
+
+	for (int a = 0; a < n; a++) {
+		arr[a] = 0;
+	}
+
+	int i, j, k;
+	for (int a = 0; a < m; a++) {
+		cin >> i >> j >> k;
+		for (int b = i; b <= j; b++) {
+			arr[b - 1] = k;
+		}
+	}
+	for (int a = 0; a < n; a++) {
+		cout << arr[a] << " ";
+	}
+}
