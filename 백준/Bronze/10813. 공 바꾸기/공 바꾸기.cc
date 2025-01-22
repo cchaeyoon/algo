9606d1bddae1792666e53c56598282473927e431
cchaeyoon <161849407+cchaeyoon@users.noreply.github.com>
2025-01-22 16:53:16 +0900

    [Bronze II] Title: 공 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n, m;
+	cin >> n >> m;
+
+	int arr[101] = {};
+	for (int i = 0; i < 101; i++) {
+		arr[i] = i;
+	}
+
+	int a, b;
+	for (int i = 0; i < m; i++) {
+		cin >> a >> b;
+		arr[0] = arr[a];
+		arr[a] = arr[b];
+		arr[b] = arr[0];
+	}
+	for (int i = 0; i < n; i++) {
+		cout << arr[i+1] << " ";
+	}
+}
