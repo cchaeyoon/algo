1fd7a824c60e9bbf62e7464f2e8bdfa498b065e0
cchaeyoon <161849407+cchaeyoon@users.noreply.github.com>
2025-01-27 01:10:41 +0900

    [Silver II] Title: 스택 수열, Time: 16 ms, Memory: 2944 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.cc
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -9,29 +9,29 @@ int main() {
 	
 	int n;
 	cin >> n;
-	vector<int>v(n);
+	vector<int>v(n);		//입력받는 수열
 	stack <int> s;
-	int k = 0;
-	vector<char>out;
+	int k = 0;				//수열의 index값
+	vector<char>out;		//push, pop
 
 	for (int i = 0; i < n; i++) {
 		cin >> v[i];
 	}
 
-	for (int i = 1; i <= n; i++) {
+	for (int i = 1; i <= n; i++) {		//1부터 n까지를 stack에 넣음 
 		s.push(i);
 		out.push_back('+');
 
-		while (k < n && !s.empty() && s.top() == v[k]) {
+		while (k < n && !s.empty() && s.top() == v[k]) {	//이때 stack의 top이 수열의 원소와 같다면 pop하고
 			s.pop();
 			out.push_back('-');
-			k++;
+			k++;		//다음 수열의 원소 체크 (반복)
 		}
 	}
-	if (!s.empty()) {
+	if (!s.empty()) {		//stack이 안비어있으면 불가능
 		cout << "NO";
 	}
-	else {
+	else {		//push, pop 따로 출력
 		for (int i = 0; i < out.size(); i++) {
 			cout << out[i] << '\n';
 		}
