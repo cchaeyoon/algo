f78cd324c149612f48217ab07ee947003fadc0f2
cchaeyoon <161849407+cchaeyoon@users.noreply.github.com>
2025-02-06 23:49:43 +0900

    [Gold IV] Title: 부분합, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.cc
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -15,24 +15,23 @@ int main() {
 	for (int i = 1; i <= n; i++) {
 		int a;
 		cin >> a;
-		v[i] = v[i - 1] + a;
+		v[i] = v[i - 1] + a;	// 누적합
 	}
 
 	int left = 1, sum=0;
-	vector<int> len(n + 1);
 	int minLength = n + 1;
-
-	for (int right = 0; right <= n; right++) {
+	
+	for (int right = 0; right <= n; right++) {		// right가 이동
 		sum = v[right] - v[left-1];
 
-		while (sum >= s) {
-			minLength = min(minLength, right - left + 1);
-			left++;
-			sum = v[right] - v[left - 1];
+		while (sum >= s) {	// sum이 s 이상이면
+			minLength = min(minLength, right - left + 1);	// 최소 길이 업데이트
+			left++;		// left가 이동
+			sum = v[right] - v[left - 1];	// sum 업데이트
 		}
 	}
 
-	if (minLength == n + 1) {
+	if (minLength == n + 1) {		// 최소 길이가 그대로면 0 출력
 		cout << 0;
 	}
 	else {
